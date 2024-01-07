90add2c00fd859f5ae68a69d6a474fef1b8a6400
Syed Rajib Rahman <srajibrahman1@gmail.com>
2024-01-06 21:55:33 -0500

    If else example 2

---
+++ src/IfElseExample2.java
@@ -0,0 +1,12 @@
+public class IfElseExample2 {
+    public static void main(String[] args) {
+
+        double temp = 103.5;
+
+        if (temp > 100) {
+            System.out.println("You have fever.");
+          } else {
+            System.out.println("You don't have fever.");
+        }
+    }
+}
